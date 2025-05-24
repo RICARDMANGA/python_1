e3239086202d1b6534504c0f27c0a5858a90d563
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-05-24 17:28:01 +0000

    Take Conditional Input from user in Python

---
+++ hello2.py
@@ -0,0 +1,15 @@
+# Prompting the user for input
+age_input = input("Enter your age: ")
+
+# Converting the input to an integer
+age = int(age_input)
+
+# Checking conditions based on user input
+if age < 0:
+    print("Please enter a valid age.")
+elif age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
